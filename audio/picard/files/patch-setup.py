29774d61563ffaa7a977f790ff491b9eea7996cc
BSDKaffee <jhale@FreeBSD.org>
2018-03-15 05:48:55 -0400

    Update to 2.0.0.dev4

--- audio/picard/files/patch-setup.py
+++ audio/picard/files/patch-setup.py
@@ -1,8 +1,8 @@
 Don't install picard.in
 
---- setup.py.orig	2018-02-17 17:56:58 UTC
+--- setup.py.orig	2018-03-14 13:35:41 UTC
 +++ setup.py
-@@ -793,6 +793,5 @@ if py2exe is None and do_py2app is False:
+@@ -776,6 +776,5 @@ if py2exe is None and do_py2app is False:
      args['data_files'].append(('share/icons/hicolor/256x256/apps', ['resources/images/256x256/picard.png']))
      args['data_files'].append(('share/icons/hicolor/scalable/apps', ['resources/img-src/picard.svg']))
      args['data_files'].append(('share/applications', ('picard.desktop',)))
